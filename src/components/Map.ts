162677477ac4ca6f79b86cb85b500cd5e545bb10
gurkanozil <gurkanozil@live.nl>
2025-02-24 07:00:30 +0100

    Add trees

--- src/components/Map.ts
+++ src/components/Map.ts
@@ -1,9 +1,40 @@
+import type { Row } from "../types";
 import * as THREE from "three";
 import { Grass } from "./Grass";
+import { Tree } from "./Tree";
+
+export const metadata: Row[] = [
+  {
+    type: "forest",
+    trees: [
+      { tileIndex: -3, height: 50 },
+      { tileIndex: 2, height: 30 },
+      { tileIndex: 5, height: 50 },
+    ],
+  },
+];
 
 export const map = new THREE.Group();
 
 export function initializeMap() {
   const grass = Grass(0);
   map.add(grass);
+  addRows();
+}
+
+export function addRows() {
+  metadata.forEach((rowData, index) => {
+    const rowIndex = index + 1;
+
+    if (rowData.type === "forest") {
+      const row = Grass(rowIndex);
+
+      rowData.trees.forEach(({ tileIndex, height }) => {
+        const three = Tree(tileIndex, height);
+        row.add(three);
+      });
+
+      map.add(row);
+    }
+  });
 }
