d517aab5bffd746ed9c19cb8a77635cd7ba3eec8
gurkanozil <gurkanozil@live.nl>
2025-02-24 06:57:10 +0100

    Add lights to scene and change player

--- src/components/Player.ts
+++ src/components/Player.ts
@@ -3,14 +3,14 @@ import * as THREE from "three";
 export const player = Player();
 
 function Player() {
-  const player = new THREE.Group();
-
   const body = new THREE.Mesh(
     new THREE.BoxGeometry(15, 15, 20),
-    new THREE.MeshLambertMaterial({ color: "white" })
+    new THREE.MeshLambertMaterial({
+      color: "white",
+      flatShading: true,
+    })
   );
   body.position.z = 10;
-  player.add(body);
 
-  return player;
+  return body;
 }
--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,15 @@ import "./style.css";
 const scene = new THREE.Scene();
 scene.add(player);
 
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
+
+const dirLight = new THREE.DirectionalLight();
+dirLight.position.set(-100, -100, 200);
+scene.add(dirLight);
+
 const camera = Camera();
-player.add(camera);
+scene.add(camera);
 
 const renderer = Renderer();
 renderer.render(scene, camera);
