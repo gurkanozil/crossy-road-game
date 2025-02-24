f429b6e81e83294aa9904fd96da379985ee6ffb3
gurkanozil <gurkanozil@live.nl>
2025-02-24 06:59:23 +0100

    Render map and add grass

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import * as THREE from "three";
 import { Renderer } from "./components/Renderer";
 import { Camera } from "./components/Camera";
 import { player } from "./components/Player";
+import { map, initializeMap } from "./components/Map";
 import "./style.css";
 
 const scene = new THREE.Scene();
 scene.add(player);
+scene.add(map);
 
 const ambientLight = new THREE.AmbientLight();
 scene.add(ambientLight);
@@ -16,6 +18,12 @@ scene.add(dirLight);
 
 const camera = Camera();
 scene.add(camera);
+
+initializeGame();
+
+function initializeGame() {
+  initializeMap();
+}
 
 const renderer = Renderer();
 renderer.render(scene, camera);
